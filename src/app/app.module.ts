1b50b03beae24d620405606fe4887d7a59f0946b
eddyizm <eddyizm@gmail.com>
2020-04-03 13:50:01 -0700

    created form and wired up form control. routed to weather component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
+import { RouterModule } from '@angular/router';
+import { allAppRoutes } from './routes';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { WeatherComponent } from './weather/weather.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(allAppRoutes),
+    ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder,FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
+
 export class WeatherComponent implements OnInit {
+  public weatherSearchForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.weatherSearchForm = this.formBuilder.group(
+      {
+        location: ['']
+      }
+    );
   }
 
 }
