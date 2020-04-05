c6de30a3f68a02152d97740d5eeb4369696f9cb9
eddyizm <eddyizm@gmail.com>
2020-04-04 17:02:07 -0700

    added github pages, changed from weather stack to open weather api

--- src/app/apixu.service.ts
+++ src/app/apixu.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-// http://api.weatherstack.com/current?access_key=dac0476d1dbd3b280410bd617cbe6d82&query=Fullerton
-const BASE_URL = 'http://api.weatherstack.com/current?access_key='
-const YOUR_API_KEY = ''
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?q='
+const WEATHER = ''
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +12,6 @@ export class ApixuService {
   constructor(private http: HttpClient) { }
 
   getWeather(location){
-    return this.http.get(BASE_URL + YOUR_API_KEY + '&query=' + location);
+    return this.http.get(BASE_URL + location + '&units=imperial&appid=' + WEATHER);
   }
 }
